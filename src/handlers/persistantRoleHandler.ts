47b967418dd2abc5c0538b2d0564fc2a67b2e3ef
Maximus7474 <the.maximus.7474@gmail.com>
2026-02-26 00:20:46 +0100

    feat(handlers): persitant role handler

---
+++ src/handlers/persistantRoleHandler.ts
@@ -0,0 +1,46 @@
+import { DiscordAPIError, type Guild } from 'discord.js';
+import { PersisantRoles } from '../constants';
+import logger from '../utils/logger';
+import db from '../utils/db';
+import { persistentRoles } from '../utils/db/schema';
+import { and, eq } from 'drizzle-orm';
+
+export async function persistantRoleHandler(guild: Guild, userid: string, roleKey: keyof typeof PersisantRoles, action: "add" | "remove") {
+    const roleId = PersisantRoles[roleKey];
+    if (!roleId) {
+        throw new Error(`Role ID for key "${roleKey}" not found in constants.`);
+    }
+
+    try {
+        if (action === "add") {
+            await db.insert(persistentRoles)
+                .values({ userId: userid, roleId: roleId })
+                .onDuplicateKeyUpdate({ set: { userId: userid } });
+        } else {
+            await db.delete(persistentRoles)
+                .where(and(
+                    eq(persistentRoles.userId, userid),
+                    eq(persistentRoles.roleId, roleId)
+                ));
+        }
+
+        try {
+            const member = await guild.members.fetch(userid);
+
+            if (action === "add") {
+                await member.roles.add(roleId);
+            } else {
+                await member.roles.remove(roleId);
+            }
+        } catch (error) {
+            if ((error as DiscordAPIError).code === 10007) {
+                logger.debug(`User ${userid} not in guild; updated database only.`);
+            } else {
+                throw error;
+            }
+        }
+    } catch (error) {
+        logger.error(`Failed to ${action} persistent role for ${userid}:`, error);
+        throw error;
+    }
+}
